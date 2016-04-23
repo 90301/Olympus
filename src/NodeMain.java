281ac3204f7a2608484692debda10733f0ebbf2c
Josh <josh.benton.ce@gmail.com>
2016-04-23 00:29:14 -0400

    set up loops

--- src/NodeMain.java
+++ src/NodeMain.java
@@ -2,25 +2,24 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
 
 /**
- * This class is run on a node in the simulation
- * It is intended to have methods to start the simulation,
- * and hold top level datastructures for that node.
+ * This class is run on a node in the simulation It is intended to have methods
+ * to start the simulation, and hold top level datastructures for that node.
+ * 
  * @author inhaler
  *
  */
 public class NodeMain {
-	//Simulation variables and parameters
+	// Simulation variables and parameters
 	public static String simulationState;
 	public static final String SIMULATION_RUNNING = "Running";
 	public static final String SIMULATION_PAUSED = "Paused";
 	public static final String SIMULATION_ENDING = "Ending";
 	private static final int TOP_LEVEL_STRUCTURES = 100;
-	
-	//Top level datastructures
-	
-	public static ConcurrentHashMap<String, City> cities = new ConcurrentHashMap<String,City>();
-	
-	
+
+	// Top level datastructures
+
+	public static ConcurrentHashMap<String, City> cities = new ConcurrentHashMap<String, City>();
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		startSimulation();
@@ -28,21 +27,17 @@ public class NodeMain {
 		mainSimulation();
 
 	}
+
 	/**
-	 * Looping code that keeps running until the program is
-	 * done running.
+	 * Looping code that keeps running until the program is done running.
 	 */
 	public static void mainSimulation() {
-		 while (simulationState != SIMULATION_ENDING) {
-			if (simulationState==SIMULATION_RUNNING) {
-			 //main similation code
-				
-				
-				
-				
-				
-			} else if (simulationState==SIMULATION_PAUSED) {
-				//simulation "paused" code
+		while (simulationState != SIMULATION_ENDING) {
+			if (simulationState == SIMULATION_RUNNING) {
+				// main simulation code
+				cities.values().parallelStream().forEach(s -> s.simulateStep());
+			} else if (simulationState == SIMULATION_PAUSED) {
+				// simulation "paused" code
 				try {
 					TimeUnit.SECONDS.sleep(5);
 				} catch (InterruptedException e) {
@@ -50,34 +45,34 @@ public class NodeMain {
 					e.printStackTrace();
 				}
 			}
-			 
-		 }
+
+		}
 	}
-	
-	//Generation code
+
+	// Generation code
 	public static void generate() {
-		for (int i=0;i<TOP_LEVEL_STRUCTURES;i++) {
+		for (int i = 0; i < TOP_LEVEL_STRUCTURES; i++) {
 			City c = new City();
 			c.generateCity(100);
 			cities.put(c.getId(), c);
 		}
 	}
-	
+
 	private static long nextId = 0;
+
 	public static String genID() {
 		System.out.println(nextId);
-		return ""+nextId++;
+		return "" + nextId++;
 	}
-	
+
 	/**
-	 * This starts the simulation.
-	 * TODO:
-	 * Make this work with remote method invocation
-	 * to start simulations on multiple nodes.
+	 * This starts the simulation. TODO: Make this work with remote method
+	 * invocation to start simulations on multiple nodes.
 	 */
 	public static void startSimulation() {
 		simulationState = SIMULATION_RUNNING;
 	}
+
 	/**
 	 * 
 	 */
--- src/Person.java
+++ src/Person.java
@@ -5,6 +5,9 @@
  */
 public class Person implements simulateable {
 	
+	private static final double MAX_BASE_MONEY = 100;
+	private static final double MAX_BASE_AGE = 100;
+	private static final int MAX_AGE = 1000;
 	private String name;
 	private String id;
 	private int money;
@@ -19,13 +22,23 @@ public class Person implements simulateable {
 	
 	
 	public void simulateStep() {
+		this.age ++;
+		//simulate the person here
 		
+		if (age > MAX_AGE) {
+			//die
+		}
 	}
-
+	/**
+	 * This method generates people at the start of the simulation
+	 * Another method should be used for generating a person during
+	 * the simulation.
+	 */
 	public void generatePerson() {
 		this.id = NodeMain.genID();
 		this.name = "Person: " + this.id;
-		this.age = (int) (Math.random()*100);
+		this.age = (int) (Math.random()*MAX_BASE_AGE);
+		this.money = (int) (Math.random()*MAX_BASE_MONEY);
 	}
 
 	@Override
